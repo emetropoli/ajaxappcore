3f585b63a226062b7e68f27851f0b925cdbffaee
Jhon Murillo <jhon.murillo@interpagos.local>
2018-02-26 07:59:34 -0500

    Agregar archivos de proyecto.

---
+++ ajaxappcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ajaxappcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
